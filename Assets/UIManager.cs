4c016d57f852a4f5a28f33bfbb8866a078d70e7b
Kieran Hislop <kieranjhislop@gmail.com>
2025-02-05 15:24:55 -0400

    Testing some stuff

--- Assets/UIManager.cs
+++ Assets/UIManager.cs
@@ -12,7 +12,7 @@ public class UIManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        score = 0;      
+        score = 0;
     }
 
     public void addScore()
